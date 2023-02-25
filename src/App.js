ab062969228ff8e4d00046965604cb5f1a3292a3
nikhilambati <nikhilambati02@gmail.com>
2023-02-25 20:57:13 +0530

    tiktok clone

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Video from './components/Video';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className='app__videos'>
+      <Video 
+        url="https://res.cloudinary.com/dxkxvfo2o/video/upload/v1608169738/video1_cvrjfm.mp4"
+        channel="nikhilambati"
+        description="Macbook Air to new Windows editing beast"
+        likes = {234}
+        shares={245}
+        messages={890}/>
+
+        <Video 
+        url="https://res.cloudinary.com/dxkxvfo2o/video/upload/v1608169739/video2_mecbdo.mp4"
+        channel="ambatiNikhil"
+        description="Technology is the booming business in todays age"
+        likes = {256}
+        shares={254}
+        messages={900}/>
+      
+      <Video 
+        url="https://res.cloudinary.com/dxkxvfo2o/video/upload/v1608169738/video1_cvrjfm.mp4"
+        channel="nikhilambati"
+        description="Macbook Air to new Windows editing beast"
+        likes = {234}
+        shares={245}
+        messages={890}/>
+
+<Video 
+        url="https://res.cloudinary.com/dxkxvfo2o/video/upload/v1608169739/video2_mecbdo.mp4"
+        channel="ambatiNikhil"
+        description="Technology is the booming business in todays age"
+        likes = {286}
+        shares={345}
+        messages={870}/>
+      </div>
     </div>
   );
 }
